af5b642f46535399c61cacf308a872a33cb6f553
Lennart Kloock <lennart.kloock@protonmail.com>
2021-10-09 13:07:26 +0200

    Edit build script Windows version does not show cmd when executed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 case "$1" in
 windows)
-  GOARCH=amd64 GOOS=windows go build -o build/win/TLS-Fingerprint-API.exe
+  GOARCH=amd64 GOOS=windows go build -ldflags -H=windowsgui -o build/win/TLS-Fingerprint-API.exe
   ;;
 linux)
   GOARCH=amd64 GOOS=linux go build -o build/linux/TLS-Fingerprint-API
